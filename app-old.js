e780edcf0d4fd73699d18f54625d5cc5d255e4e1
Alexis <luis.sosa@interware.com.mx>
2021-03-21 12:21:27 -0600

    se crear webserver para app estatica

---
+++ app-old.js
@@ -0,0 +1,13 @@
+const http =require('http');
+
+http.createServer((req,res)=>{
+   // res.writeHead(200,{'Content-Type':'application/json'});
+    // res.setHeader('Content-Disposition','attachment; filename=lista.csv')
+    // res.writeHead(200,{'Content-Type':'application/csv'});
+
+
+    res.write('hola mundo');
+    res.end();
+}).listen(8080)
+
+console.log('Escuchando en el puerto',8080)
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+require('dotenv').config(); 
+
+const port =process.env.PORT;
+//handeblars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+//HBS RENDERIZA LAS VISTAS DESDE EL LADO DEL BACKEND
+
+//SERVIR CONTENIDO ESTATICO
+app.use(express.static('public'));
+ //POR ESO LA RUTA PADRE / NO SE EJECUTA,INMEDIATAMENTE SE OBSERVA  EL CONTNEIDO DEL HTML
+
+ app.get('/',  (req, res)=> {
+    res.render('home',{
+        nombre:'Alexis Biñuelo',
+        titulo:'Curso de Node'
+    })
+  })
+
+app.get('/generic',  (req, res)=> {
+    res.render('generic',{
+        nombre:'Alexis Biñuelo',
+        titulo:'Curso de Node'
+    })
+  })
+
+
+  app.get('/elements',  (req, res)=> {
+    res.render('elements',{
+        nombre:'Alexis Biñuelo',
+        titulo:'Curso de Node'
+    })
+  })
+
+  app.get('*', (req, res)=> {
+    res.sendFile(__dirname+'/public/404.html')
+  })
+ 
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
